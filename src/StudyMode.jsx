ca0afe3186ffd3bdc47cd3c59283fad96d8d819b
Илья Шестак <ilia1259697@gmail.com>
2026-05-08 12:11:39 +0300

    deck display

--- src/StudyMode.jsx
+++ src/StudyMode.jsx
@@ -1,8 +1,44 @@
 import React from "react";
+import Deck from "./Deck";
+import Card from "./Card";
 
 class Study extends React.Component {
+  state = {
+    decks: [new Deck("name")],
+  };
+
+  componentDidMount() {
+    const data = localStorage.getItem("decks");
+    this.setState({ decks: data ? JSON.parse(data) : [] });
+  }
   render() {
-    return <button>sdada</button>;
+    return (
+      <>
+        <div style={{ display: "flex", gap: "10px" }}>
+          {this.state.decks.map((deck, index) => {
+            return (
+              <div key={index}>
+                <button
+                  onClick={() => {
+                    this.setState({
+                      activeDeckNumber: index,
+                      activeDeck: this.state.decks[index],
+                    });
+                  }}
+                >
+                  {deck.name}
+                </button>
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+      </>
+    );
   }
 }
 
